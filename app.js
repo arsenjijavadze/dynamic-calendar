0b606175da48ddba023a33175a941b48290c6254
Arsen Jijavadze <arsen.jijavadze@gmail.com>
2022-12-22 16:20:10 +0300

    feat: add click event on prew and next icon and get correct value of days in current month

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const currentDate = document.querySelector('.current-date');
+const currentDate = document.querySelector('.current-date'),
+    daysTag = document.querySelector('.days'),
+    prevNextIcon = document.querySelectorAll('.icons span');
 
 // getting new date, current year and month
 let date = new Date(),
@@ -10,12 +12,20 @@ const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
 const renderCalendar = () => {
     //* getDate() returns the day of the month (1 to 31) of a date
     let lastDateOfMonth = new Date(currYear, currMonth + 1, 0).getDate(); // getting last date of month
+    let liTag = '';
 
-    for (let i = 0; i < array.length; i++) {
-        const element = array[i];
+    for (let i = 1; i <= lastDateOfMonth; i++) {
+        liTag += `<li>${i}</li>`;
 
     }
 
     currentDate.innerText = `${months[currMonth]} ${currYear}`;
+    daysTag.innerHTML = liTag;
 }
 renderCalendar();
+
+prevNextIcon.forEach(icon => {
+    icon.addEventListener('click', () => { //adding click event on both icons
+        console.log(icon);
+    })
+});
